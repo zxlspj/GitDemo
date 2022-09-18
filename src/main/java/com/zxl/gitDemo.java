8a31928d745cdb0212035f1c20e40e548ef4c88d
zxl <3088536856@qq.com>
2022-09-18 11:29:36 +0800

    third commit in IDEA

--- src/main/java/com/zxl/gitDemo.java
+++ src/main/java/com/zxl/gitDemo.java
@@ -4,5 +4,7 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello again");
+        System.out.println("hello again again");
+        System.out.println("hello again again again");
     }
 }
