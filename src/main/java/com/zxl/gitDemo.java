23ca65232e750ca2a3b9150bdf11edad0505997f
zxl <3088536856@qq.com>
2022-09-18 16:32:31 +0800

    second commit in hot-fix

--- src/main/java/com/zxl/gitDemo.java
+++ src/main/java/com/zxl/gitDemo.java
@@ -2,7 +2,6 @@ package com.zxl;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello again");
+        System.out.println("second commit in hot-fix");
     }
 }
