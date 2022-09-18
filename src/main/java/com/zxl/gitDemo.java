e3b7b3c533cd782f1111e01129518a0856d7e2a0
zxl <3088536856@qq.com>
2022-09-18 16:35:49 +0800

    third commit in master

--- src/main/java/com/zxl/gitDemo.java
+++ src/main/java/com/zxl/gitDemo.java
@@ -2,9 +2,6 @@ package com.zxl;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello again");
-        System.out.println("hello again again");
-        System.out.println("hello again again again");
+        System.out.println("third commit in master");
     }
 }
