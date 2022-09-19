04ce7a6a00d48f9595de52d6106310fb52de0568
zxlspj <75550855+zxlspj@users.noreply.github.com>
2022-09-19 17:11:35 +0800

    Update gitDemo.java

--- src/main/java/com/zxl/gitDemo.java
+++ src/main/java/com/zxl/gitDemo.java
@@ -2,7 +2,6 @@ package com.zxl;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("在本地库做了修改");
-        System.out.println("又在本地库做了一次修改~~~");
+        System.out.println("直接在远程库做了一次修改~~~");
     }
 }
