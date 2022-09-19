2b5c287783ee73bd2b567c5476fdac22c6f0dae5
zxl <3088536856@qq.com>
2022-09-19 15:42:08 +0800

    9-19 commit in hot-fix

--- src/main/java/com/zxl/gitDemo.java
+++ src/main/java/com/zxl/gitDemo.java
@@ -2,7 +2,6 @@ package com.zxl;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("在master分支上做了一次修改");
-        System.out.println("9-19 commit in hot-fix");
+        System.out.println("在本地库做了修改");
     }
 }
