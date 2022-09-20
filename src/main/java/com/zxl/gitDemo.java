fd2fe2298d4ff4bad5a5cc695dc720414ef8663b
zxlspj <75550855+zxlspj@users.noreply.github.com>
2022-09-20 10:07:33 +0800

    9月20日 在远程库中做了一次修改

--- src/main/java/com/zxl/gitDemo.java
+++ src/main/java/com/zxl/gitDemo.java
@@ -3,6 +3,6 @@ package com.zxl;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("直接在远程库做了一次修改~~~");
-        System.out.println("9月20日 在远程库中做了一次修改");
+        System.out.println("9月20日 在远程库中做了一次修改  ");
     }
 }
